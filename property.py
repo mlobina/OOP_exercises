e455b844773219f994a548797e8d6326a920b9c3
Marina Lobina <mvlobina@gmsil.com>
2021-11-17 16:07:27 +0300

    update property (3 - use property decorators)

--- property.py
+++ property.py
@@ -5,27 +5,29 @@ class ChildSmith:
         self.__name = name
         self.__gender = gender
 
-    def __get_name(self):
+    @property
+    def name(self):
         return self.__name
 
-    def __get_gender(self):
-        return self.__gender
-
-    def __set_name(self, value):
+    @name.setter
+    def name(self, value):
         self.__name = value
 
-    def __set_gender(self, value):
+    @property
+    def gender(self):
+        return self.__gender
+
+    @gender.setter
+    def gender(self, value):
         self.__gender = value
 
-    def __get_surname(self):
+    @property
+    def surname(self):
         return self.__SURNAME
 
-    def __change_surname(self, value):
+    @surname.setter
+    def surname(self, value):
         self.__SURNAME = value
-
-    name = property(fget=__get_name, fset=__set_name)
-    gender = property(fget=__get_gender, fset=__set_gender)
-    surname = property(fget=__get_surname, fset=__change_surname)
 
 
 petr = ChildSmith("Petr", "male")
